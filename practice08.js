4ff2c6c200c513f1f093a121c76b8db4cb49e04c
zhangshuanyu <849125536@qq.com>
2017-08-16 16:54:50 +0800

    找出数组中重复出现过的元素

---
+++ practice08.js
@@ -0,0 +1,16 @@
+/*20170816
+/题目：找出数组 arr 中重复出现过的元素
+例如：arr = [‘a’, ‘ab’, ‘ed’, ‘da’, ‘a’, ‘ed’, ‘b’];输出：‘a’, ‘ed’
+ */
+
+arr = ['a','ab','ed','da','a','ed','b'];
+function a(arr) {
+    var result = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === arr[i + 1]) {
+            result.push(arr[i]);
+        }
+    }
+    console.log('数组：['+arr+']中重复出现的元素有：['+result+']');
+}
+a(arr);
